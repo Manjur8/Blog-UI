239938423bc6af7f20dcffaf712ab9510e73ba71
Manjur <manjursk209@gmail.com>
2024-01-30 22:10:11 +0530

    dev - project setup & initialization

---
+++ src/features/home/index.js
@@ -0,0 +1,82 @@
+import { Card, Flex } from 'antd'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='p-16'>
+        <Flex
+        wrap='wrap'
+        align="center"
+        justify="center"
+        gap='small'
+        // style={{
+        //   padding: 32,
+        // }}
+      >
+
+<Card
+            title="Home Page"
+            bordered={false}
+            style={{
+            width: 240,
+            }}
+        >
+            <p>lorem ipsum sldkl is ind jds kjds. His ksk skns ..</p>
+        </Card>
+
+        <Card
+            title="Home Page"
+            bordered={false}
+            style={{
+            width: 240,
+            }}
+        >
+            <p>lorem ipsum sldkl is ind jds kjds. His ksk skns ..</p>
+        </Card>
+
+        <Card
+            title="Home Page"
+            bordered={false}
+            style={{
+            width: 240,
+            }}
+        >
+            <p>lorem ipsum sldkl is ind jds kjds. His ksk skns ..</p>
+        </Card>
+
+        <Card
+            title="Home Page"
+            bordered={false}
+            style={{
+            width: 240,
+            }}
+        >
+            <p>lorem ipsum sldkl is ind jds kjds. His ksk skns ..</p>
+        </Card>
+
+        <Card
+            title="Home Page"
+            bordered={false}
+            style={{
+            width: 240,
+            }}
+        >
+            <p>lorem ipsum sldkl is ind jds kjds. His ksk skns ..</p>
+        </Card>
+
+        <Card
+            title="Home Page"
+            bordered={false}
+            style={{
+            width: 240,
+            }}
+        >
+            <p>lorem ipsum sldkl is ind jds kjds. His ksk skns ..</p>
+        </Card>
+      </Flex>
+        
+    </div>
+  )
+}
+
+export default Home
